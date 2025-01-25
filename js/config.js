1ca9a5faee2d307b6bace2f9b57cd5487d3bef65
fkv747 <villanuevakevin08@outlook.com>
2025-01-25 06:13:02 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_n9pluWT7n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '76migd3dee7r5p0cfob8vst2sq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
